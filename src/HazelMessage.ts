90a45a2870983cac1ad3d791dd27f8ecd3238bd9
martinGITHUBER <maroymart@gmail.com>
2021-09-16 13:35:59 -0400

    Bug fix for offset in reliable packet

--- src/HazelMessage.ts
+++ src/HazelMessage.ts
@@ -6,13 +6,13 @@ export default class HazelMessage {
 	payload: Buffer;
 
 	constructor(tag: number, payload: Buffer) {
-		this.length = 3 + payload.length;
+		this.length = payload.length;
 		this.tag = tag;
 		this.payload = payload;
 	}
 
 	public serialize(): Buffer {
-		const buffer = Buffer.alloc(this.length);
+		const buffer = Buffer.alloc(3 + this.length);
 		buffer.writeUInt16BE(this.length, 0);
 		buffer.writeUInt8(this.tag, 2);
 		this.payload.copy(buffer, 3);
@@ -21,6 +21,6 @@ export default class HazelMessage {
 
 	public static deserialize(buffer: Buffer): HazelMessage {
 		const length = buffer.readUInt16BE(0);
-		return new HazelMessage(buffer.readUInt8(2), buffer.length > 3 ? buffer.slice(3, length) : Buffer.alloc(0));
+		return new HazelMessage(buffer.readUInt8(2), buffer.length > 3 ? buffer.slice(3, 3 + length) : Buffer.alloc(0));
 	}
 }
--- src/packets/ReliablePacket.ts
+++ src/packets/ReliablePacket.ts
@@ -1,6 +1,6 @@
 import { Bound, PacketType } from "../enums";
 import HazelMessage from "../HazelMessage";
-import { uint16, uint32 } from "../types/numbers";
+import { uint16 } from "../types/numbers";
 import Packet from "./Packet";
 
 export default class ReliablePacket extends Packet {
@@ -17,8 +17,8 @@ export default class ReliablePacket extends Packet {
 	public serialize(): Buffer {
 		const buffer: Buffer = Buffer.alloc(5 + this.payloads.reduce((a, b) => a + b.serialize().length, 0));
 		buffer.writeUInt8(ReliablePacket.type, 0);
-		buffer.writeUInt32LE(this.nonce, 1);
-		let offset: number = 5;
+		buffer.writeUInt16BE(this.nonce, 1);
+		let offset: number = 3;
 		for(const payloads of this.payloads) {
 			payloads.serialize().copy(buffer, offset);
 			offset += payloads.serialize().length;
@@ -27,13 +27,13 @@ export default class ReliablePacket extends Packet {
 	}
 
 	public static deserialize(buffer: Buffer, bound: Bound): ReliablePacket {
-		const nonce: uint32 = buffer.readUInt32LE(1);
-		const payload: Array<HazelMessage> = [];
-		let offset: number = 5;
+		const nonce: uint16 = buffer.readUInt16BE(1);
+		const payloads: Array<HazelMessage> = [];
+		let offset: number = 3;
 		while(offset < buffer.length) {
-			payload.push(HazelMessage.deserialize(buffer.slice(offset)));
-			offset += payload[payload.length - 1].serialize().length;
+			payloads.push(HazelMessage.deserialize(buffer.slice(offset)));
+			offset += payloads[payloads.length - 1].serialize().length;
 		}
-		return new ReliablePacket(nonce, payload, bound);
+		return new ReliablePacket(nonce, payloads, bound);
 	}
 }
