eb54b1fbf77bf778482663b021ca7cc45f028973
martinGITHUBER <maroymart@gmail.com>
2021-09-16 14:59:40 -0400

    Implemented GameList and IPv4

---
+++ src/messages/GameList.ts
@@ -0,0 +1,40 @@
+import { byte, int32, uint16, uint32 } from "../types/numbers";
+import IPv4 from "../utils/IPv4";
+
+export default class GameList {
+	public ip: IPv4;
+	public port: uint16;
+	public game_id: int32;
+	public host_name: string;
+	public player_count: byte;
+	public age: uint32;
+	public map_id: byte;
+	public impostor_count: byte;
+	public max_players: byte;
+
+	public constructor(ip: IPv4, port: uint16, game_id: int32, host_name: string, player_count: byte, age: uint32, map_id: byte, impostor_count: byte, max_players: byte) {
+		this.ip = ip;
+		this.port = port;
+		this.game_id = game_id;
+		this.host_name = host_name;
+		this.player_count = player_count;
+		this.age = age;
+		this.map_id = map_id;
+		this.impostor_count = impostor_count;
+		this.max_players = max_players;
+	}
+
+	public serialize(): Buffer {
+		const buffer = Buffer.alloc(19 + this.host_name.length);
+		buffer.copy(this.ip.serialize(), 0);
+		buffer.writeUInt16LE(this.port, 4);
+		buffer.writeInt32LE(this.game_id, 6);
+		buffer.write(this.host_name, 10, "utf8");
+		buffer.writeUInt8(this.player_count, 10 + this.host_name.length);
+		buffer.writeUInt32LE(this.age, 11 + this.host_name.length);
+		buffer.writeUInt8(this.map_id, 15 + this.host_name.length);
+		buffer.writeUInt8(this.impostor_count, 16 + this.host_name.length);
+		buffer.writeUInt8(this.max_players, 17 + this.host_name.length);
+		return buffer;
+	}
+}
---
+++ src/utils/IPv4.ts
@@ -0,0 +1,37 @@
+import { byte, uint32 } from "../types/numbers";
+
+export default class IPv4 {
+	public a: byte;
+	public b: byte;
+	public c: byte;
+	public d: byte;
+
+	public constructor(a: byte, b: byte, c: byte, d: byte) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+
+	public serialize(): Buffer {
+		const buffer = Buffer.alloc(4);
+		buffer.writeUInt8(this.a, 0);
+		buffer.writeUInt8(this.b, 1);
+		buffer.writeUInt8(this.c, 2);
+		buffer.writeUInt8(this.d, 3);
+		return buffer;
+	}
+
+	public to_string(): string {
+		return `${this.a}.${this.b}.${this.c}.${this.d}`;
+	}
+
+	public static deserialize(buffer: Buffer): IPv4 {
+		return new IPv4(buffer.readUInt8(0), buffer.readUInt8(1), buffer.readUInt8(2), buffer.readUInt8(3));
+	}
+
+	public static from_string(ip: string): IPv4 {
+		const [a, b, c, d] = ip.split(".");
+		return new IPv4(parseInt(a), parseInt(b), parseInt(c), parseInt(d));
+	}
+}
